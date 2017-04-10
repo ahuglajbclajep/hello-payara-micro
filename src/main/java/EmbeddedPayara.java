54340dd31111de11ad48e20ec5632565ea514c54
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-04-11 08:23:35 +0900

    add gradle wrapper, close #1

---
+++ src/main/java/EmbeddedPayara.java
@@ -0,0 +1,10 @@
+import fish.payara.micro.BootstrapException;
+import fish.payara.micro.PayaraMicro;
+
+public class EmbeddedPayara
+{
+    public static void main(String[] args) throws BootstrapException
+    {
+        PayaraMicro.getInstance().bootStrap();
+    }
+}
