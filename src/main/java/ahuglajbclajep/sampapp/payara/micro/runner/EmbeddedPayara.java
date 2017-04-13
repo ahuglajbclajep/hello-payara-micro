0fbe4e7e8978c3ee6f8c39472921d5c4ec2aaf03
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-04-13 18:58:48 +0900

    upd output log

--- src/main/java/ahuglajbclajep/sampapp/payara/micro/runner/EmbeddedPayara.java
+++ src/main/java/ahuglajbclajep/sampapp/payara/micro/runner/EmbeddedPayara.java
@@ -1,10 +1,10 @@
 package ahuglajbclajep.sampapp.payara.micro.runner;
 
-import fish.payara.micro.BootstrapException;
 import fish.payara.micro.PayaraMicro;
+import fish.payara.micro.BootstrapException;
 
 public class EmbeddedPayara {
-     public static void main(String[] args) throws BootstrapException {
-        PayaraMicro.getInstance().bootStrap();
-    }
+	public static void main(String[] args) throws BootstrapException {
+		PayaraMicro.getInstance().bootStrap();
+	}
 }
